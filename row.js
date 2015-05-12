2db5f2c3d93e6279a2454b753a8aed526e5c3cd9
Reid Philpot <reidphilpot@gmail.com>
2015-05-12 21:21:16 +0100

    tabs to spaces

--- row.js
+++ row.js
@@ -1,41 +1,41 @@
 define(
-	[ 'd3'
-	, 'underscore'
-	]
-	, function (d3, _) {
+  [ 'd3'
+  , 'underscore'
+  ]
+  , function (d3, _) {
 
-		var value = _.property('value')
-		, width = _.property('width')
+    var value = _.property('value')
+      , width = _.property('width')
 
-		return function rowFactory() {
+    return function rowFactory() {
 
-			var columns = {}
+      var columns = {}
 
-			function row(s) {
-				s.each(rowEach)
-			}
+      function row(s) {
+        s.each(rowEach)
+      }
 
-			row.columns = function (v) {
-				if (!arguments.length) return columns
-				columns = v
-				return row
-			}
+      row.columns = function (v) {
+        if (!arguments.length) return columns
+        columns = v
+        return row
+      }
 
-			function rowEach(d) {
-				var target = d3.select(this)
-					, values = columns.map(function (v) { return _.extend(v, { value: d[v.field] }) }) 
-					, cells = target.selectAll('div.cell').data(values)
+      function rowEach(d) {
+        var target = d3.select(this)
+          , values = columns.map(function (v) { return _.extend(v, { value: d[v.field] }) }) 
+          , cells = target.selectAll('div.cell').data(values)
 
-				cells.enter().append('div').classed('cell', true)
-				cells.exit().remove()
+        cells.enter().append('div').classed('cell', true)
+        cells.exit().remove()
 
-				cells
-					.text(value)  
-					.style('width', width)
-			}
+        cells
+          .text(value)  
+          .style('width', width)
+      }
 
-			return row
-		}
+      return row
+    }
 
-	}
+  }
 )
